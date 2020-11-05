a1d154bd8bf47bb8b114ad94fb4757068d5abf9f
wss-git <wssgryx@163.com>
2020-11-05 16:49:41 +0800

    remove tags

---
+++ utils/client.js
@@ -0,0 +1,95 @@
+'use strict'
+
+const FC = require('@alicloud/fc2');
+const Pop = require('@alicloud/pop-core');
+const Logger = require('./logger');
+const ServerlessError = require('./error');
+
+FC.prototype.getAccountSettings = function (options = {}, headers = {}) {
+  return this.get('/account-settings', options, headers)
+}
+
+class Client {
+  constructor (credentials, region) {
+    this.region = region;
+    this.credentials = credentials;
+
+    this.accountId = credentials.AccountID;
+    this.accessKeyID = credentials.AccessKeyID;
+    this.accessKeySecret = credentials.AccessKeySecret;
+    this.stsToken = credentials.SecurityToken;
+
+    this.logger = new Logger();
+  }
+
+  buildOssClient (bucketName) {
+    return new OSS({
+      region: this.region,
+      accessKeyId: this.accessKeyID,
+      accessKeySecret: this.accessKeySecret,
+      stsToken: this.stsToken,
+      bucket: bucketName
+    })
+  }
+
+  buildFcClient () {
+    return new FC(this.accountId, {
+      accessKeyID: this.accessKeyID,
+      accessKeySecret: this.accessKeySecret,
+      securityToken: this.stsToken,
+      region: this.region,
+      timeout: 6000000
+    })
+  }
+
+  buildVpcClient () {
+    return new Pop({
+      endpoint: 'https://vpc.aliyuncs.com',
+      apiVersion: '2016-04-28',
+      accessKeyId: this.accessKeyID,
+      accessKeySecret: this.accessKeySecret,
+      opts: {
+        timeout: 60 * 1000
+      }
+    })
+  }
+
+  buildEcsClient () {
+    return new Pop({
+      endpoint: 'https://ecs.aliyuncs.com',
+      apiVersion: '2014-05-26',
+      accessKeyId: this.accessKeyID,
+      accessKeySecret: this.accessKeySecret,
+      opts: {
+        timeout: 60 * 1000
+      }
+    })
+  }
+
+  buildNasClient () {
+    return new Pop({
+      endpoint: `http://nas.${this.region}.aliyuncs.com`,
+      apiVersion: '2017-06-26',
+      accessKeyId: this.accessKeyID,
+      accessKeySecret: this.accessKeySecret,
+      opts: {
+        timeout: 60 * 1000
+      }
+    })
+  }
+
+  buildRoaClient () {
+    return new ROAClient({
+      accessKeyId: this.accessKeyID,
+      accessKeySecret: this.accessKeySecret,
+      endpoint: `https://cr.${this.region}.aliyuncs.com`,
+      apiVersion: '2016-06-07'
+    })
+  }
+
+  throwError (e) {
+    new ServerlessError(e);
+  }
+}
+
+module.exports = Client
--- utils/help.js
+++ utils/help.js
@@ -1,6 +1,32 @@
 
 module.exports = (inputs) => ({
-  description: `Usage: s ${inputs.Project.ProjectName} metrics
+  description: `Usage: s ${inputs.Project.ProjectName} remove [command]
 
-  Monitoring function indicators.`
+    Delete application.`,
+  commands: [{
+    name: 'function',
+    desc: 'only remove function.'
+  }, {
+    name: 'tags',
+    desc: 'only remove service tags.'
+  }, {
+    name: 'tags -k, --key <name>',
+    desc: 'only the specified service tag are remove.'
+  }, {
+    name: 'domain',
+    desc: 'only remove domain.'
+  }, {
+    name: 'domain -d, --domain <name>',
+    desc: 'only remove the specified domain name.'
+  }, {
+    name: 'trigger',
+    desc: 'only remove trigger.'
+  }, {
+    name: 'trigger -n, --name <name>',
+    desc: 'only remove the specified trigger name.'
+  }],
+  args: [{
+    name: '-f/--force',
+    desc: 'delete auto generated resource by force.'
+  }]
 })
